9f1093c8c5fc769c89ffc591f0b5be42e9a8ecfa
MikeWatts <watts.mike@heb.com>
2021-03-19 09:28:11 -0500

    Create aws automation for s3

---
+++ Voidwalker/terraform/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+    region = "us-east-1"
+    shared_credentials_file = "/Users/w175613/.aws/credentials"
+    profile = "personal"
+}
---
+++ Voidwalker/terraform/route53.tf
@@ -0,0 +1,2 @@
+### Create some automation around creating domain creation for voidwalker.dev ###
+### Also, create some automation around cloudfront CDN service. ###
